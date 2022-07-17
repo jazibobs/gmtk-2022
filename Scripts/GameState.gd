1024f67464a822146067a206bb9a43aeee8aa49d
Jared Rigby <talk@jaredrigby.co.uk>
2022-07-17 18:32:21 +0100

    Updated game to use the correct initial state

--- Scripts/GameState.gd
+++ Scripts/GameState.gd
@@ -2,8 +2,8 @@ extends Node
 
 var win_state = false
 var timer_active = false
-var active_rooms = [true, true, true, true, true, false]
-var complete_rooms = [true, true, true, true, true, false]
+var active_rooms = [false, false, false, false, false, false]
+var complete_rooms = [false, false, false, false, false, false]
 var current_health = 3
 var show_player_ui = false
 
